6724de12292cf77428dd9d7b9bcf62904c7819a1
Nereol <pimboliwell.fr@gmail.com>
2024-10-06 20:57:25 +0200

    modified the arborescence to be able to serve the index.html page (ex main_page.html)

--- Portfolio/dynamic/carousel.js
+++ Portfolio/dynamic/carousel.js
@@ -1,35 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Fetch the CSV file
-    fetch('../media/project.csv')
-    .then(response => response.text())
+    const carousel = document.querySelector('#carousel');
+    let currentIndex=0;
+
+    fetch('../media/liste_projet.json')
+    .then(response => response.json())
     .then(data => {
-        const rows = data.trim().split('\n'); // Split data into rows
-        const carousel = document.querySelector('#carousel');
-        let currentIndex = 0;
-
-        for (let i = 1; i < rows.length; i++) {
-            const row = rows[i];
-            const columns = row.split(';'); // Split each row into columns using semicolon as separator
-            const shorttexte = columns[3].trim(); // Get the image column and remove whitespace
-            const image = columns[4].trim(); // Get the image column and remove whitespace
-            const favoris = columns[6].trim(); // Get the favorite column and remove whitespace
-
-            if (favoris !=='') { // Check if the favoris column is not empty
+        data.forEach(element => {
+            if (element.Favoris){
                 const card=document.createElement('div');
                 card.classList.add('card');
                 carousel.appendChild(card);
                 const img = document.createElement('img');
-                img.src = image;
+                img.src = element.Images;
                 img.width="800";
                 img.height="500";
                 img.alt = "Image carousel";
                 card.appendChild(img);
                 const text = document.createElement('p');
-                text.textContent = shorttexte;
+                text.textContent = element.Description;
                 card.appendChild(text);
             }
-        };
-
+        });
         const prevButton = document.createElement('button');
         prevButton.classList.add('prev-button');
         prevButton.innerHTML='<i class="fa-solid fa-chevron-left fa-2xl"></i>';
